ab871bf870d4ca8679674101512c78fb1fc3cc76
syh2020 <syh754200@g.skku.edu>
2021-05-22 19:45:43 +0900

    crawling completed, but not tagging

--- lib.js
+++ lib.js
@@ -1,93 +1,195 @@
+const http = require("http");
 const https = require("https");
 const parser = require("node-html-parser");
 const navigator = require("navigator");
 const userAgent = navigator.userAgent;
+const defaulturl = "https://skb.skku.edu/haksaeng/status.do";
 
-/*
-구현해야 하는 것.
-1. #번째 사람을 찾기.
- - 해당 사람의 동선이 기록된 링크를 찾기
- - 
-2. 해당 사람의 동선 파악.
-3. 해당 동선에 따라 지도에 선 긋기.
-*/
+// from https://usefulangle.com/post/170/nodejs-synchronous-http-request
 
-function getLastUrl(url) {
-    https.get(
-        url,
-        {
-            headers: {
-                "User-Agent": userAgent,
+// 마지막 페이지의 url을 반환하는 함수.
+function getPromise_LastUrl() {
+    return new Promise((resolve, reject) => {
+        https.get(
+            "https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offse",
+            {
+                headers: {
+                    "User-Agent": userAgent,
+                },
             },
-        },
-        (res) => {
-            let data = "";
+            (res) => {
+                let data = [];
 
-            res.on("data", (d) => {
-                data += d;
-            });
-            res.on("end", () => {
-                let root = parser.parse(data);
-                let lastHtml = root.querySelectorAll(".page-last");
-                console.log(lastHtml);
-                //console.log(data);
-                // 여기서 href를 return 할 예정.
-            });
-        }
-    );
+                res.on("data", (fragments) => {
+                    data.push(fragments);
+                });
+
+                res.on("end", () => {
+                    let root = parser.parse(data);
+                    let lastHtml = root.querySelectorAll(".page-last");
+
+                    ret = defaulturl + lastHtml[0]._attrs.href;
+                    //console.log(ret);
+                    resolve(ret);
+                });
+
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            }
+        );
+    });
 }
 
-/* 웹페이지를 들어가보면 1번page에 75~60번 확진자, 2번page에 59~45번 확진자 이런식으로 정보가 들어있다. 여기서 target
- * 을 정해서 찾으면 그 동선을 return하는 함수이다.
- */
-function findPerson(num) {
-    let basicUrl =
+// async function to make http request
+async function makeSynchronousRequest_LastUrl(request) {
+    try {
+        let http_promise = getPromise_LastUrl();
+        let last_url = await http_promise;
+
+        // holds response from server that is passed when Promise is resolved
+        return last_url;
+    } catch (error) {
+        // Promise rejected
+        console.log(error);
+    }
+}
+
+// target 사람의 url을 반환하는 함수
+function getPromise_FindPersonUrl(url, num) {
+    return new Promise((resolve, reject) => {
+        https.get(
+            url,
+            {
+                headers: {
+                    "User-Agent": userAgent,
+                },
+            },
+            (res) => {
+                let data = [];
+
+                res.on("data", (fragments) => {
+                    data.push(fragments);
+                });
+
+                res.on("end", () => {
+                    let root = parser.parse(data);
+                    let ret = "";
+                    root.querySelectorAll(
+                        "dt.board-list-content-title"
+                    ).forEach((d) => {
+                        if (d.innerText.indexOf(String(num)) != -1) {
+                            let splitedUrl = d.childNodes[1].rawAttrs
+                                .split('"')[1]
+                                .replace(/amp/g, "article")
+                                .replace(/;article/g, "");
+                            resolve(defaulturl + splitedUrl);
+                        }
+                        //?mode=view&amp;articleNo=115397&amp;article.offset=0&amp;articleLimit=10"
+                        //?mode=view&articleNo=115397&article.offset=0&articleLimit=10
+                        //?mode=view&articleNo=115397&article.offset=0&articleLimit=10
+                        ret += d.innerText.trim() + "\n";
+                    });
+                    //console.log(ret);
+                    resolve(-1);
+                });
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            }
+        );
+    });
+}
+
+// async function to make http request
+async function makeSynchronousRequest_FindPersonUrl(url, num, request) {
+    try {
+        let http_promise = getPromise_FindPersonUrl(url, num);
+        let personUrl = await http_promise;
+
+        // holds response from server that is passed when Promise is resolved
+        return personUrl;
+    } catch (error) {
+        // Promise rejected
+        console.log(error);
+    }
+}
+
+// 특정사람의 path를 반환하는 함수
+function getPromise_FindPath(url, num) {
+    return new Promise((resolve, reject) => {
+        https.get(
+            url,
+            {
+                headers: {
+                    "User-Agent": userAgent,
+                },
+            },
+            (res) => {
+                let data = [];
+
+                res.on("data", (fragments) => {
+                    data.push(fragments);
+                });
+
+                res.on("end", () => {
+                    let root = parser.parse(data);
+                    let ret = "";
+                    root.querySelectorAll(".fr-view").forEach((d) => {
+                        ret += d;
+                    });
+                    resolve(ret);
+                });
+
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            }
+        );
+    });
+}
+
+// async function to make http request
+async function makeSynchronousRequest_FindPath(url, request) {
+    try {
+        let http_promise = getPromise_FindPath(url);
+        let path = await http_promise;
+
+        // holds response from server that is passed when Promise is resolved
+        return path;
+    } catch (error) {
+        // Promise rejected
+        console.log(error);
+    }
+}
+
+// anonymous async function to execute some code synchronously after http request
+const findPersonv = async function (num) {
+    // wait to http request to finish
+    const basicUrl =
         "https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offset";
     let i = 0;
     let curUrl = basicUrl + "=" + i;
-    let lastUrl = getLastUrl(curUrl); // Prediction: https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offset=50
-    // 각 웹페이지마다 title에서 number를 확인해서 있으면 해당 href를 반환한다.
-    // 없으면 -1을 반환한다.
-    do {
+    let last_url = await makeSynchronousRequest_LastUrl();
+    console.log(curUrl);
+    let targeturl = await makeSynchronousRequest_FindPersonUrl(curUrl, num);
+    if (targeturl != -1) {
+        let path = await makeSynchronousRequest_FindPath(targeturl);
+        // 이부분을 return으로 교체
+        console.log(path);
+    }
+
+    while (curUrl !== last_url) {
         i += 10;
-        /*
-         * if(title에 num이 있으면) {
-         *  return getPath(해당 href, num);
-         * }
-         */
         curUrl = basicUrl + "=" + i;
-    } while (curUrl !== lastUrl);
-}
-
-// #번 확진자의 대한 정보가 담긴 url으로부터 동선정보를 return하는 함수.
-function getPath(url, num) {
-    https.get(
-        url,
-        {
-            headers: {
-                "User-Agent": userAgent,
-            },
-        },
-        (res) => {
-            let data = "";
-
-            res.on("data", (d) => {
-                data += d;
-            });
-            res.on("end", () => {
-                let root = parser.parse(data);
-                root.querySelectorAll(".fr-view").forEach((d) => {
-                    console.log(d.innerText.trim());
-                });
-                // 이후 이부분을 return으로 바꾸고 이를 html에 추가하는 형식이 필요.
-                //console.log(data);
-            });
+        targeturl = await makeSynchronousRequest_FindPersonUrl(curUrl, num);
+        if (targeturl != -1) {
+            let path = await makeSynchronousRequest_FindPath(targeturl);
+            // 이부분을 return으로 교체
+            console.log(path);
         }
-    );
-}
+    }
+};
+findPersonv(57);
 
-// main.js에서 작동시킬 functions를 export하기.
-
-//getLastUrl("https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offset=0");
-
-module.exports = {};
+module.exports = { findPersonv };
