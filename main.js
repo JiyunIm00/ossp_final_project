1ef4a4f2c1084a3d56d4259ed47faf5d099881d0
Jiyun Im <ellejiyunim@gmail.com>
2021-05-24 23:40:02 +0900

    merge

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 // 이동경로 관련 정보를 함수는 lib.js로 이동해주세요.
 const lib = require("./lib.js");
 
-async function main() {
+async function main(){
     var person = await lib.makeSynchronousRequest_FindPerson(66);
-
-    console.log(person.splitedData);
+    console.log(person.path);
 }
-main();
+
