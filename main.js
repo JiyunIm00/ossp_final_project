895b2470e3f765d59883761785a20e5b007cbb59
Jiyun Im <ellejiyunim@gmail.com>
2021-05-24 23:41:24 +0900

    merge

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 // 이부분은 html eventhandler를 다루는 파일입니다.
 // 이동경로 관련 정보를 함수는 lib.js로 이동해주세요.
 const lib = require("./lib.js");
-
 async function main(){
     var person = await lib.makeSynchronousRequest_FindPerson(66);
     console.log(person.path);
 }
-
