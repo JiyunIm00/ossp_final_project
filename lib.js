ce4b84323760c8f5a0ad6a2c4f17cf068eb7bc9c
xhmxnjxx <ominj98@g.skku.edu>
2021-05-24 12:50:30 +0900

    updated tagging

--- lib.js
+++ lib.js
@@ -244,6 +244,7 @@ async function makeSynchronousRequest_FindPerson(num, request) {
     }
 }
 
+<<<<<<< HEAD
 // main function for debugging
 async function main() {
     /*
@@ -257,3 +258,223 @@ async function main() {
 
 main();
 module.exports = { makeSynchronousRequest_FindPerson };
+=======
+// target 사람이 포함된 포스트 내에 게시된 총 확진자의 수*/를 반환하는 함수
+function getPromise_FindTotalNumInPost(url, targetNum) {
+    return new Promise((resolve, reject) => {
+        https.get(
+            url,
+            {
+                headers: {
+                    "User-Agent": userAgent,
+                },
+            },
+            (res) => {
+                let data = [];
+
+                res.on("data", (fragments) => {
+                    data.push(fragments);
+                });
+
+                res.on("end", () => {
+                    let root = parser.parse(data);
+                    let ret = "";
+                    root.querySelectorAll(
+                        "dt.board-list-content-title"
+                    ).forEach((d) => {
+                        let splitedStr = d.innerText
+                            .trim()
+                            .replace(/(\r\n\t|\n|\r\t)/gm, "")
+                            .split("~");
+
+                        if (splitedStr.length === 1) {
+                            if (d.innerText.indexOf(String(targetNum)) !== -1) {
+                                resolve(1);
+                            }
+                        } else {
+                            let num1 = parseInt(
+                                splitedStr[0].substring(
+                                    splitedStr[0].indexOf(" ") + 1,
+                                    splitedStr[0].length
+                                )
+                            );
+                            let num2 = parseInt(
+                                splitedStr[1].substring(
+                                    0,
+                                    splitedStr[1].indexOf("번")
+                                )
+                            );
+                            //console.log(num1 + " " + num2);
+                            for (let num = num1; num <= num2; num++) {
+                                if (num === targetNum) {
+                                    /* target 사람이 포함된 포스트 내에 게시된 총 확진자의 수*/
+                                    let totalNumInPost = (num2 - num1) + 1;
+                                    //console.log(totalNumInPost) 
+                                    resolve(totalNumInPost);
+                                }
+                            }
+                        }
+                    });
+                    resolve(-1);
+                });
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            }
+        );
+    });
+}
+
+// async function to make http request
+async function makeSynchronousRequest_FindTotalNumInPost(url, num) {
+    try {
+        let http_promise = getPromise_FindTotalNumInPost(url, num);
+        let totalNumInPost = await http_promise;
+
+        // holds response from server that is passed when Promise is resolved
+        return totalNumInPost;
+    } catch (error) {
+        // Promise rejected
+        console.log(error);
+    }
+}
+
+// async function to make http request
+const findTotalNumInPost = async function(num) {
+    const basicUrl =
+        "https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offset";
+    let i = 0;
+    let curUrl = basicUrl + "=" + i;
+    let last_url = await makeSynchronousRequest_LastUrl();
+
+    let totalNumInPost = await makeSynchronousRequest_FindTotalNumInPost(curUrl, num);
+    if(totalNumInPost !== -1) {
+        //이 부분을 return으로 대체
+        console.log(totalNumInPost);
+    }
+
+    while(curUrl !== last_url) {
+        i += 10;
+        curUrl = basicUrl + "=" + i;
+        totalNumInPost = await makeSynchronousRequest_FindTotalNumInPost(curUrl, num);
+        if(totalNumInPost !== -1) {
+            //이 부분을 return으로 대체
+            console.log(totalNumInPost);
+        }
+    }
+}
+
+// 포스트 내 target 사람의 순서
+function getPromise_FindTargetSequence(url, targetNum) {
+    return new Promise((resolve, reject) => {
+        https.get(
+            url,
+            {
+                headers: {
+                    "User-Agent": userAgent,
+                },
+            },
+            (res) => {
+                let data = [];
+
+                res.on("data", (fragments) => {
+                    data.push(fragments);
+                });
+
+                res.on("end", () => {
+                    let root = parser.parse(data);
+                    let ret = "";
+                    root.querySelectorAll(
+                        "dt.board-list-content-title"
+                    ).forEach((d) => {
+                        let splitedStr = d.innerText
+                            .trim()
+                            .replace(/(\r\n\t|\n|\r\t)/gm, "")
+                            .split("~");
+
+                        if (splitedStr.length === 1) {
+                            if (d.innerText.indexOf(String(targetNum)) !== -1) {
+                                resolve(1);
+                            }
+                        } else {
+                            let num1 = parseInt(
+                                splitedStr[0].substring(
+                                    splitedStr[0].indexOf(" ") + 1,
+                                    splitedStr[0].length
+                                )
+                            );
+                            let num2 = parseInt(
+                                splitedStr[1].substring(
+                                    0,
+                                    splitedStr[1].indexOf("번")
+                                )
+                            );
+                            //console.log(num1 + " " + num2);
+                            for (let num = num1; num <= num2; num++) {
+                                if (num === targetNum) {
+                                    /* target 사람이 포함된 Post에 게시된 총 확진자의 수 */
+                                    let targetSequence = (num - num1) + 1;
+                                    //console.log(targetSequence) 
+                                    resolve(targetSequence);
+                                }
+                            }
+                        }
+                    });
+                    resolve(-1);
+                });
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            }
+        );
+    });
+}
+
+// async function to make http request
+async function makeSynchronousRequest_FindTargetSequence(url, num) {
+    try {
+        let http_promise = getPromise_FindTargetSequence(url, num);
+        let targetSequence = await http_promise;
+
+        // holds response from server that is passed when Promise is resolved
+        return targetSequence;
+    } catch (error) {
+        // Promise rejected
+        console.log(error);
+    }
+}
+
+// async function to make http request
+const findTargetSequence = async function(num) {
+    const basicUrl =
+        "https://skb.skku.edu/haksaeng/status.do?mode=list&&articleLimit=10&article.offset";
+    let i = 0;
+    let curUrl = basicUrl + "=" + i;
+    let last_url = await makeSynchronousRequest_LastUrl();
+
+    let targetSequence = await makeSynchronousRequest_FindTargetSequence(curUrl, num);
+    if(targetSequence !== -1) {
+        //이 부분을 return으로 대체
+        //return targetSequence;
+        console.log(targetSequence);
+    }
+
+    while(curUrl !== last_url) {
+        i += 10;
+        curUrl = basicUrl + "=" + i;
+        targetSequence = await makeSynchronousRequest_FindTargetSequence(curUrl, num);
+        if(targetSequence !== -1) {
+            //이 부분을 return으로 대체
+            // eturn targetSequence;
+            console.log(targetSequence);
+        }
+    }
+}
+
+
+findPersonv(69);
+findTotalNumInPost(69);
+findTargetSequence(69).then()
+console.log(targetSequence);
+module.exports = { findPersonv , findTotalNumInPost, findTargetSequence};
+>>>>>>> 8bb6588 (updated tagging)
